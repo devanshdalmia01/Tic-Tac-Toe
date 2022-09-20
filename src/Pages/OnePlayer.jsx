c1651a61fd1070b0c5dc63098d5d7feb04dad5e6
devanshdalmia01 <devanshdalmia1@gmail.com>
2022-09-20 17:56:48 +0530

    finalising

--- src/Pages/OnePlayer.jsx
+++ src/Pages/OnePlayer.jsx
@@ -2,49 +2,67 @@ import Base from "../Base";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser, resetGame, computerChance } from "../redux/storingData";
 import Container from "../Components/Container";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function OnePlayer() {
 	const [onePlayer] = useState(true);
 	const user1 = useSelector((state) => state["user1"]);
 	const user2 = useSelector((state) => state["user2"]);
+	const moves = useSelector((state) => state["moves"]);
+	const winnerDeclared = useSelector((state) => state["winnerDeclared"]);
 	const dispatch = useDispatch();
+	useEffect(() => {
+		if (winnerDeclared) {
+			setTimeout(() => {
+				dispatch(resetGame());
+			}, 2000);
+		}
+	}, [winnerDeclared]);
 	return (
 		<Base>
 			<main className="mainData">
-				{user1[1] && user2[1] ? (
+				{moves === 9 && !winnerDeclared ? (
+					<>
+						<h1 className="whosChance">Unfortunately no one won!</h1>
+					</>
+				) : user1[1] && user2[1] ? (
 					<>
 						<Container onePlayer={onePlayer} />
-						<h1>{user1[0] ? "Chance - User" : "Chance - Computer"}</h1>
+						<h1 className="whosChance">{user1[0] ? "Chance - User" : "Chance - Computer"}</h1>
 						<div
+							className="resetButton"
 							onClick={() => {
 								dispatch(resetGame());
 							}}
 						>
-							Reset
+							Reset Game
 						</div>
 					</>
 				) : (
 					<>
-						What do you want to choose? X or O?
-						<div
-							onClick={() => {
-								dispatch(setUser(["user1", [true, "X"]]));
-								dispatch(setUser(["user2", [false, "O"]]));
-							}}
-						>
-							X
-						</div>
-						<div
-							onClick={() => {
-								dispatch(setUser(["user1", [false, "O"]]));
-								dispatch(setUser(["user2", [true, "X"]]));
-								setTimeout(() => {
-									dispatch(computerChance());
-								}, 2000);
-							}}
-						>
-							O
+						<h1>What do you want to choose? X or O?</h1>
+						<div className="buttonContainer">
+							<div
+								className="selectXorO"
+								onClick={() => {
+									dispatch(setUser(["user1", [true, "X"]]));
+									dispatch(setUser(["user2", [false, "O"]]));
+								}}
+							>
+								X
+							</div>
+							<div
+								className="selectXorO"
+								onClick={() => {
+									dispatch(setUser(["user1", [false, "O"]]));
+									dispatch(setUser(["user2", [true, "X"]]));
+									setTimeout(() => {
+										dispatch(computerChance());
+									}, 2000);
+								}}
+							>
+								O
+							</div>
 						</div>
 					</>
 				)}
--- src/redux/storingData.js
+++ src/redux/storingData.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 import getRandom from "../Utilities/Random";
 
 const dataSlice = createSlice({
@@ -12,6 +13,8 @@ const dataSlice = createSlice({
 		user1: [false, ""],
 		user2: [false, ""],
 		moves: 0,
+		winnerDeclared: false,
+		lastFilled: "",
 	},
 	reducers: {
 		registerChance(state, action) {
@@ -21,15 +24,18 @@ const dataSlice = createSlice({
 					state["user1"][0] = !state["user1"][0];
 					state["user2"][0] = !state["user2"][0];
 					state["moves"] = state["moves"] + 1;
+					state["lastFilled"] = action.payload[1];
 					break;
 				case "user2":
 					state[0][action.payload[1][0]][action.payload[1][1]] = state["user2"][1];
 					state["user2"][0] = !state["user2"][0];
 					state["user1"][0] = !state["user1"][0];
 					state["moves"] = state["moves"] + 1;
+					state["lastFilled"] = action.payload[1];
 					break;
 				default:
 			}
+			dataSlice.caseReducers.winningLogic(state, action);
 		},
 		setUser(state, action) {
 			switch (action.payload[0]) {
@@ -55,13 +61,103 @@ const dataSlice = createSlice({
 						}
 					} else return num;
 				});
-				console.log(grid);
 				const val = grid[getRandom(grid)];
-				console.log(val);
 				state[0][val[0]][val[1]] = state["user2"][1];
 				state["user2"][0] = !state["user2"][0];
 				state["user1"][0] = !state["user1"][0];
 				state["moves"] = state["moves"] + 1;
+				state["lastFilled"] = val;
+				dataSlice.caseReducers.winningLogic(state, action);
+			}
+		},
+		winningLogic(state, action) {
+			switch (state["lastFilled"]) {
+				case "00":
+					if (
+						(state[0][0][0] === state[0][0][1] && state[0][0][1] === state[0][0][2]) ||
+						(state[0][0][0] === state[0][1][0] && state[0][1][0] === state[0][2][0]) ||
+						(state[0][0][0] === state[0][1][1] && state[0][1][1] === state[0][2][2])
+					) {
+						state["user1"][1] === state[0][0][0] && toast.success(`${state["user1"][1]} is winner.`);
+						state["user2"][1] === state[0][0][0] && toast.success(`${state["user2"][1]} is winner.`);
+						state["winnerDeclared"] = true;
+					}
+					break;
+				case "01":
+					if ((state[0][0][0] === state[0][0][1] && state[0][0][1] === state[0][0][2]) || (state[0][0][1] === state[0][1][1] && state[0][1][1] === state[0][2][1])) {
+						state["user1"][1] === state[0][0][1] && toast.success(`${state["user1"][1]} is winner.`);
+						state["user2"][1] === state[0][0][1] && toast.success(`${state["user2"][1]} is winner.`);
+						state["winnerDeclared"] = true;
+					}
+					break;
+				case "02":
+					if (
+						(state[0][0][0] === state[0][0][1] && state[0][0][1] === state[0][0][2]) ||
+						(state[0][0][2] === state[0][1][2] && state[0][1][2] === state[0][2][2]) ||
+						(state[0][0][2] === state[0][1][1] && state[0][1][1] === state[0][2][0])
+					) {
+						state["user1"][1] === state[0][0][2] && toast.success(`${state["user1"][1]} is winner.`);
+						state["user2"][1] === state[0][0][2] && toast.success(`${state["user2"][1]} is winner.`);
+						state["winnerDeclared"] = true;
+					}
+					break;
+				case "10":
+					if ((state[0][1][0] === state[0][1][1] && state[0][0][1] === state[0][1][2]) || (state[0][0][0] === state[0][1][0] && state[0][1][0] === state[0][2][0])) {
+						state["user1"][1] === state[0][1][0] && toast.success(`${state["user1"][1]} is winner.`);
+						state["user2"][1] === state[0][1][0] && toast.success(`${state["user2"][1]} is winner.`);
+						state["winnerDeclared"] = true;
+					}
+					break;
+				case "11":
+					if (
+						(state[0][0][0] === state[0][1][1] && state[0][1][1] === state[0][2][2]) ||
+						(state[0][0][1] === state[0][1][1] && state[0][1][1] === state[0][2][1]) ||
+						(state[0][1][0] === state[0][1][1] && state[0][1][1] === state[0][1][2]) ||
+						(state[0][0][2] === state[0][1][1] && state[0][1][1] === state[0][2][0])
+					) {
+						state["user1"][1] === state[0][1][1] && toast.success(`${state["user1"][1]} is winner.`);
+						state["user2"][1] === state[0][1][1] && toast.success(`${state["user2"][1]} is winner.`);
+						state["winnerDeclared"] = true;
+					}
+					break;
+				case "12":
+					if ((state[0][1][0] === state[0][1][1] && state[0][1][1] === state[0][1][2]) || (state[0][0][2] === state[0][1][2] && state[0][1][2] === state[0][2][2])) {
+						state["user1"][1] === state[0][1][2] && toast.success(`${state["user1"][1]} is winner.`);
+						state["user2"][1] === state[0][1][2] && toast.success(`${state["user2"][1]} is winner.`);
+						state["winnerDeclared"] = true;
+					}
+					break;
+				case "20":
+					if (
+						(state[0][0][0] === state[0][1][0] && state[0][1][0] === state[0][2][0]) ||
+						(state[0][2][0] === state[0][1][1] && state[0][1][1] === state[0][0][2]) ||
+						(state[0][2][0] === state[0][2][1] && state[0][2][1] === state[0][2][2])
+					) {
+						state["user1"][1] === state[0][2][0] && toast.success(`${state["user1"][1]} is winner.`);
+						state["user2"][1] === state[0][2][0] && toast.success(`${state["user2"][1]} is winner.`);
+						state["winnerDeclared"] = true;
+					}
+					break;
+				case "21":
+					if ((state[0][2][0] === state[0][2][1] && state[0][2][1] === state[0][2][2]) || (state[0][2][1] === state[0][1][1] && state[0][1][1] === state[0][0][1])) {
+						state["user1"][1] === state[0][2][1] && toast.success(`${state["user1"][1]} is winner.`);
+						state["user2"][1] === state[0][2][1] && toast.success(`${state["user2"][1]} is winner.`);
+						state["winnerDeclared"] = true;
+					}
+					break;
+				case "22":
+					if (
+						(state[0][0][0] === state[0][1][1] && state[0][1][1] === state[0][2][2]) ||
+						(state[0][2][0] === state[0][2][1] && state[0][2][1] === state[0][2][2]) ||
+						(state[0][2][2] === state[0][1][2] && state[0][1][2] === state[0][0][2])
+					) {
+						state["user1"][1] === state[0][2][2] && toast.success(`${state["user1"][1]} is winner.`);
+						state["user2"][1] === state[0][2][2] && toast.success(`${state["user2"][1]} is winner.`);
+						state["winnerDeclared"] = true;
+					}
+					break;
+
+				default:
 			}
 		},
 		resetGame(state, action) {
@@ -72,10 +168,13 @@ const dataSlice = createSlice({
 			];
 			state["user1"] = [false, ""];
 			state["user2"] = [false, ""];
+			state["moves"] = 0;
+			state["winnerDeclared"] = false;
+			state["lastFilled"] = "";
 		},
 	},
 });
 
-export const { registerChance, setUser, computerChance, resetGame } = dataSlice.actions;
+export const { registerChance, setUser, computerChance, winningLogic, resetGame } = dataSlice.actions;
 
 export default dataSlice.reducer;
